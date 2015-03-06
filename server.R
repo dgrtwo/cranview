ad99366a0686c04002290ec175db6d343c925941
dgrtwo <dgrtwo@princeton.edu>
2015-03-06 14:48:22 -0500

    Initial commit of CRAN viewer app

---
+++ server.R
@@ -0,0 +1,52 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(cranlogs)
+
+# if package not found, keep the same one up
+last <- NULL
+last_package <- ""
+
+shinyServer(function(input, output) {
+  downloads <- reactive({
+      end_date <- Sys.Date() - 1
+      start_date <- end_date - input$num_days + 1
+      
+      ret <- cran_downloads(package = input$package,
+                            from = start_date,
+                            to = end_date)
+      if (is.null(ret$downloads)) {
+          return(last)
+      }
+      d <- ret$downloads %>% mutate(day = as.Date(day))
+      last <<- d
+      last_package <<- input$package
+      d
+  })
+    
+  output$downloadsPlot <- renderPlot({
+      d <- downloads()
+      if (input$by_week) {
+          d <- d %>%
+              mutate(date = floor_date(day, "week")) %>%
+              group_by(date) %>%
+              summarize(downloads = sum(downloads))
+      } else {
+          d <- d %>% rename(date = day)
+      }
+
+      ggplot(d, aes(date, downloads)) + geom_line() +
+          ggtitle(paste("Downloads for", last_package)) +
+          xlab("Date") +
+          ylab("Number of downloads")
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,38 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Package Downloads Over Time"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      HTML("Enter an R package to see the # of downloads over time from the RStudio CRAN Mirror."),
+      textInput("package", "Package:", "shiny"),
+      # submitButton("View Downloads"),
+      sliderInput("num_days",
+                  "Number of days:",
+                  min = 1,
+                  max = 90,
+                  value = 30),
+      checkboxInput("by_week", "Summarize by week", value = TRUE),
+      HTML("Created using the <a href='https://github.com/metacran/cranlogs'>cranlogs</a> package.",
+           "This app is not affiliated with RStudio or CRAN.",
+           "You can find the code for the app <a href='https://github.com/dgrtwo/cranview'>here</a>,",
+           "or read more about it <a href='http://varianceexplained.org/r/cran-view/'>here</a>.")
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("downloadsPlot")
+    )
+  )
+))
